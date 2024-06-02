24d5611389ec15619d757bca86fdf90ad9ba0630
Marcel Wiget <mwiget@gmail.com>
2024-06-02 12:27:17 +0200

    qm import requires full path to qcow2 image

--- create_f5xc_ce_template.sh
+++ create_f5xc_ce_template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# adjust path to downloaded qcow2 file, target template id and storage ...
-qcow2=./rhel-9.2024.11-20240523024833.qcow2
+# adjust full path to downloaded qcow2 file, target template id and storage ...
+qcow2=/root/rhel-9.2024.11-20240523024833.qcow2
 id=9000
 storage=cephpool
 
@@ -13,6 +13,5 @@ qm create $id --memory 16384 --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci
 qm set $id --name f5xc-ce-template
 qm set $id --scsi0 $storage:0,import-from=$qcow2
 qm set $id --boot order=scsi0
-#qm set $id --ide2 local-lvm:cloudinit # to be created via terraform ...
 qm set $id --serial0 socket --vga serial0
 qm template $id
