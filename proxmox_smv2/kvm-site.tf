47f99f0748c550149f2bb299b2781997ca082aae
Marcel Wiget <mwiget@gmail.com>
2024-06-25 15:24:20 +0200

    3-node v2 site

--- proxmox_smv2/kvm-site.tf
+++ proxmox_smv2/kvm-site.tf
@@ -5,7 +5,7 @@ resource "restapi_object" "kvm-site" {
 }
 
 locals {
-  kvm-site = jsonencode({
+  kvm-site = var.master_node_count == 1 ? jsonencode({
     "metadata": {
       "name": var.f5xc_cluster_name,
       "disable": false
@@ -52,6 +52,54 @@ locals {
         "geo_proximity": {}
       },
       "disable_ha": {}
+    }
+  }) : jsonencode({
+    "metadata": {
+      "name": var.f5xc_cluster_name,
+      "disable": false
+    },
+    "spec": {
+      "kvm": {
+        "not_managed": {}
+      },
+      "no_network_policy": {},
+      "no_forward_proxy": {},
+      "software_settings": {
+        "sw": {
+          "default_sw_version": {}
+        },
+        "os": {
+          "default_os_version": {}
+        }
+      },
+      "upgrade_settings": {
+        "kubernetes_upgrade_drain": {
+          "disable_upgrade_drain": {}
+        }
+      },
+      "logs_streaming_disabled": {},
+      "block_all_services": {},
+      "performance_enhancement_mode": {
+        "perf_mode_l7_enhanced": {}
+      },
+      "offline_survivability_mode": {
+        "no_offline_survivability_mode": {}
+      },
+      "tunnel_dead_timeout": 0,
+      "load_balancing": {
+        "vip_vrrp_mode": "VIP_VRRP_DISABLE"
+      },
+      "no_s2s_connectivity_sli": {},
+      "no_s2s_connectivity_slo": {},
+      "local_vrf": {
+        "default_config": {},
+        "default_sli_config": {}
+      },
+      "tunnel_type": "SITE_TO_SITE_TUNNEL_IPSEC_OR_SSL",
+      "re_select": {
+        "geo_proximity": {}
+      },
+      "enable_ha": {}
     }
   })
 }
